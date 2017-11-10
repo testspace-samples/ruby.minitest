dcb4a699dd8cf55f70ff2987859d844b87a65e4f
Jeff Schultz <jschultz8960@yahoo.com>
2017-11-09 20:12:27 -0800

    update analysis script

--- run_analysis.sh
+++ run_analysis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bundle exec scss-lint --no-color --format=Stats --format=Default --out=tmp/scss-lint.txt  app/assets/stylesheets/
 bundle exec rubocop --format emacs --out tmp/rubocop.txt
+bundle exec scss-lint --no-color --format=Stats --format=Default --out=tmp/scss-lint.txt  app/assets/stylesheets/
 bundle exec brakeman -o tmp/brakeman.json
 bundle exec brakeman_translate_checkstyle_format translate --file="tmp/brakeman.json" > tmp/brakeman_checkstyle.xml
